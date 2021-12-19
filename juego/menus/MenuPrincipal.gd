e88ee0ce3777d871788d81b509a8a413981c2926
Raizu548 <rizzoluis53@gmail.com>
2021-12-18 21:01:32 -0300

    añadido Menu como jugar y conectado al menu principal

--- juego/menus/MenuPrincipal.gd
+++ juego/menus/MenuPrincipal.gd
@@ -9,3 +9,7 @@ func _ready():
 func _on_BotonIniciar_pressed():
 	MusicaGlobal.replay()
 	get_tree().change_scene(escena)
+
+
+func _on_BotonComoJugar_pressed():
+	get_tree().change_scene("res://juego/menus/MenuComoJugar.tscn")
